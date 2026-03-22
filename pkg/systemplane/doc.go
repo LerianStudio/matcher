4cb142f15a5092a88ecf2dfa3c0934b0101efdf0
Fred Amaral <fred@fredamaral.com.br>
2026-03-22 13:04:32 -0300

    refactor(systemplane): remove local systemplane package
    Deletes the entire pkg/systemplane/ directory (170+ files).
    
    All systemplane functionality has been migrated to lib-commons/v4/commons/systemplane/.
    
    This removes the embedded systemplane in favor of the shared library implementation.
    
    X-Lerian-Ref: 0x1

--- pkg/systemplane/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2025 Lerian Studio.
-
-// Package systemplane provides a backend-agnostic runtime configuration and
-// settings management library. It is designed to be reusable by any Lerian
-// product without importing internal application packages.
-package systemplane
--- pkg/systemplane/domain/actor.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2025 Lerian Studio.
-
-// Package domain defines systemplane core domain models and value objects.
-package domain
-
-// Actor identifies who made a configuration change.
-type Actor struct {
-	ID string
-}
